12f983f51ebe8cfc772f3f7e3ca295d806374e86
Marius Wichtner <mwichtner@coconut-capital.com>
2018-02-18 19:33:53 +0100

    added factory pattern

---
+++ src/main/kotlin/creational/Factory.kt
@@ -0,0 +1,16 @@
+package creational
+
+
+class Factory {
+    enum class CarType {
+        MERCEDES,
+        RENAULT,
+        PEOGEOT
+    }
+
+    fun create(type: CarType): Car = when (type) {
+        CarType.MERCEDES -> Mercedes()
+        CarType.RENAULT -> Renault()
+        CarType.PEOGEOT -> Peogeot()
+    }
+}
--- src/test/kotlin/FactoryTests.kt
+++ src/test/kotlin/FactoryTests.kt
@@ -1,4 +1,6 @@
+import creational.*
 import org.testng.Assert.assertEquals
+import org.testng.Assert.assertTrue
 import org.testng.annotations.Test
 
 class FactoryTests {
@@ -7,6 +9,14 @@ class FactoryTests {
         val mercedesFactory = MercedesFactory()
 
         val result = mercedesFactory.create().getType()
-        assertEquals("Mercedes", result)
+        assertEquals("creational.Mercedes", result)
+    }
+
+    @Test
+    fun `Factory returns correct types`() {
+        val factory = Factory()
+        assertTrue(factory.create(Factory.CarType.MERCEDES) is Mercedes)
+        assertTrue(factory.create(Factory.CarType.RENAULT) is Renault)
+        assertTrue(factory.create(Factory.CarType.PEOGEOT) is Peogeot)
     }
 }
