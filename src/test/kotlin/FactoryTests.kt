e7e9e152ef9dfbe0143e51e768e8c1f48508195f
Marius Wichtner <mwichtner@coconut-capital.com>
2018-02-18 19:35:54 +0100

    refactored factory tests

--- src/test/kotlin/FactoryTests.kt
+++ src/test/kotlin/FactoryTests.kt
@@ -5,11 +5,10 @@ import org.testng.annotations.Test
 
 class FactoryTests {
     @Test
-    fun abstractFactoryTest() {
-        val mercedesFactory = MercedesFactory()
-
-        val result = mercedesFactory.create().getType()
-        assertEquals("creational.Mercedes", result)
+    fun `AbstractFactory returns correct types`() {
+        assertTrue(MercedesFactory().create() is Mercedes)
+        assertTrue(PeogeotFactory().create() is Peogeot)
+        assertTrue(RenaultFactory().create() is Renault)
     }
 
     @Test
